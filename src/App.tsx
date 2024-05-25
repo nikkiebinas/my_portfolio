fc9bfcdb43a96bea7b79f42d9d93cbb22449f8dc
Nkkbns <104270539+Nkkbns@users.noreply.github.com>
2024-05-25 14:26:42 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,38 @@
-import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import Loader from "./loader";
-const LazyHome = lazy(() => import("@/screen/Home"));
-const LazyAbout = lazy(() => import("@/screen/About"));
-const LazyProject = lazy(() => import("@/screen/Project"));
-const LazyContact = lazy(() => import("@/screen/Contact"));
+import Home from "./screen/Home";
+import About from "./screen/About";
+import Project from "./screen/Project";
+import Contact from "./screen/Contact";
+import { useEffect, useState } from "react";
+//const LazyHome = lazy(() => import("@/screen/Home"));
+//const LazyAbout = lazy(() => import("@/screen/About"));
+//const LazyProject = lazy(() => import("@/screen/Project"));
+//const LazyContact = lazy(() => import("@/screen/Contact"));
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Suspense fallback={<Loader loading={true} />}>
-            <LazyHome />
-          </Suspense>
-        }
-      />
-      <Route
-        path="home"
-        element={
-          <Suspense fallback={<Loader loading={true} />}>
-            <LazyHome />
-          </Suspense>
-        }
-      />
-      <Route
-        path="about"
-        element={
-          <Suspense fallback={<Loader loading={true} />}>
-            <LazyAbout />
-          </Suspense>
-        }
-      />
-      <Route
-        path="project"
-        element={
-          <Suspense fallback={<Loader loading={true} />}>
-            <LazyProject />
-          </Suspense>
-        }
-      />
-      <Route
-        path="contact"
-        element={
-          <Suspense fallback={<Loader loading={true} />}>
-            <LazyContact />
-          </Suspense>
-        }
-      />
-    </Routes>
+    <>
+      {loading ? (
+        <Loader loading={loading} />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="project" element={<Project />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      )}
+    </>
   );
 }
 
