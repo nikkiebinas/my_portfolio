57632545dc309e55cca1ba3c31304af2ed6c5f64
Nikkie Binas <binasnikkie@gmail.com>
2024-07-01 03:57:55 +0800

    error fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Loader from "./Loader";
+import Loader from "./loader";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
